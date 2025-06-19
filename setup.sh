67e058f512b4e6d73334e3ca78e77f316542f3f3
abdallah ayman <161380943+Bodaxxt@users.noreply.github.com>
2025-06-20 01:21:07 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
-streamlit app.py
+#!/bin/bash
+# Force Python 3.11 environment
+pyenv install 3.11.7 -s
+pyenv global 3.11.7
+
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run app
+streamlit run app.py
