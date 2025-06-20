286a4b732781e241aca59d57c5fd361582e4e57c
abdallah ayman <161380943+Bodaxxt@users.noreply.github.com>
2025-06-20 16:28:40 +0300

    Update self_attention.py

--- self_attention.py
+++ self_attention.py
@@ -1,30 +1,41 @@
-# self_attention.py
-
 import tensorflow as tf
-from keras.saving import register_keras_serializable
+from tensorflow.keras.layers import Layer, InputSpec
 
-@register_keras_serializable()
-class SelfAttention(tf.keras.layers.Layer):
-    def __init__(self, units, **kwargs):
+class SelfAttention(Layer):
+    def __init__(self, num_heads=1, **kwargs):
         super(SelfAttention, self).__init__(**kwargs)
-        self.units = units
+        self.num_heads = num_heads
+        self.kernel = None  # Initialize kernel to None
 
-    def build(self, input_shape):
+    def build(self, input_shape): # BUILD method to create weights properly
         self.kernel = self.add_weight(
-            shape=(input_shape[-1], self.units),
-            initializer="glorot_uniform",
-            trainable=True,
-            name="kernel"
+            name='attention_kernel',
+            shape=(input_shape[-1], self.num_heads),
+            initializer='uniform',
+            trainable=True
         )
-        super().build(input_shape)
 
     def call(self, inputs):
         attention_scores = tf.matmul(inputs, self.kernel)
         attention_weights = tf.nn.softmax(attention_scores, axis=1)
-        output = tf.matmul(attention_weights, inputs)
+        
+        # Transpose inputs to make dimensions compatible
+        output = tf.matmul(attention_weights, tf.transpose(inputs, perm=[0, 2, 1]))
         return output
 
     def get_config(self):
         config = super().get_config()
-        config.update({"units": self.units})
+        config.update({'num_heads': self.num_heads}) # save num_heads
         return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config) # load num_heads
+
+    def compute_output_spec(self, input_spec):
+        if not isinstance(input_spec, InputSpec):
+            raise ValueError("This layer only supports InputSpec input.")
+        
+        shape = tf.TensorShape([input_spec.shape[0], self.num_heads, input_spec.shape[1]])
+        
+        self.output_spec = InputSpec(shape=shape)
